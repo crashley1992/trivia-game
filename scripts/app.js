385a95683a023dd100f1d8413c729978eb47d87c
Ashley Clarke <crashley92@gmail.com>
2019-08-25 20:48:35 -0700

    formatted code better

--- scripts/app.js
+++ scripts/app.js
@@ -6,118 +6,115 @@ var time = 0;
 var correct = 0;
 //stop game
 function stopGame() {
-    document.location.reload()
-  }
+  document.location.reload()
+}
 
 
 //start time button
 function startGame() {
-    start = true;
-//timer
-if (start = true) {
-    timeStart = setInterval(timer, 1000);    
-}
-//shows quiz div
-$("#start-button").hide();
-$(".display").show();
+  start = true;
+  //timer
+  if (start = true) {
+    timeStart = setInterval(timer, 1000);
+  }
+  //shows quiz div
+  $("#start-button").hide();
+  $(".display").show();
 
-//timer tracker for quiz
-function timer() {
+  //timer tracker for quiz
+  function timer() {
 
-time++;
-var converted = timeConverter(time);
-console.log(converted);
-$(".timer-display").text(converted);
-//stops timer after 20 seconds
-if (time === 20) {
-  clearInterval(timeStart);
-}
-
-
+    time++;
+    var converted = timeConverter(time);
+    console.log(converted);
+    $(".timer-display").text(converted);
+    //stops timer after 20 seconds
+    if (time === 20) {
+      clearInterval(timeStart);
+    }
 
 
-//converts time to more human readable
-function timeConverter(i) {
-var minutes = Math.floor(i / 60);
-var seconds = i - (minutes * 60);
 
-if (seconds < 10) {
-    seconds = "0" + seconds;
-}
 
-if (minutes === 0 ) {
-    minutes = "00";
-}
+    //converts time to more human readable
+    function timeConverter(i) {
+      var minutes = Math.floor(i / 60);
+      var seconds = i - (minutes * 60);
 
-else if (minutes < 10 ) {
-    minutes = "0" + minutes;
-}
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
 
-return minutes + ":" + seconds;
-}
-}
+      if (minutes === 0) {
+        minutes = "00";
+      } else if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
 
-//Question storage with questions and answers
+      return minutes + ":" + seconds;
+    }
+  }
 
-var quizQuestions = [
-{
-    question: "What year did Spongebob air for the first time?",
+  //Question storage with questions and answers
 
-    answers: {
+  var quizQuestions = [{
+      question: "What year did Spongebob air for the first time?",
+
+      answers: {
         a: "2000",
         b: "1999",
         c: "2001"
+      },
+
+      correctAnswer: "1999"
     },
 
-    correctAnswer: "1999"
-},
-
-{
-    question: "What was the first episode of Spongebob?",
-    answers: {
-      a: "Help Wanted",
-      b: "Bubble Stand",
-      c: "Jelly Fishing"
+    {
+      question: "What was the first episode of Spongebob?",
+      answers: {
+        a: "Help Wanted",
+        b: "Bubble Stand",
+        c: "Jelly Fishing"
+      },
+      correctAnswer: "Help Wanted"
     },
-    correctAnswer: "Help Wanted"
-  },
 
-  {
-    question: "Where does Sandy Cheeks Live?",
-    answers: {
-      a: "Under Water Hut",
-      b: "Texas",
-      c: "Underwater Tree Dome"
+    {
+      question: "Where does Sandy Cheeks Live?",
+      answers: {
+        a: "Under Water Hut",
+        b: "Texas",
+        c: "Underwater Tree Dome"
+      },
+      correctAnswer: "Underwater Tree Dome"
     },
-    correctAnswer: "Underwater Tree Dome"
-  },
 
-  {
-    question: "What instrument does Squidward play?",
-    answers: {
-      a: "Jazz Flute",
-      b: "Saxaphone",
-      c: "Claranet"
+    {
+      question: "What instrument does Squidward play?",
+      answers: {
+        a: "Jazz Flute",
+        b: "Saxaphone",
+        c: "Claranet"
+      },
+      correctAnswer: "Claranet"
     },
-    correctAnswer: "Claranet"
-  },
 
-];
+  ];
 
-//hides answer during queue so answer does also show in next question
-function hide() {
+  //hides answer during queue so answer does also show in next question
+  function hide() {
     $(".correct-answer").hide(quizQuestions[0].correctAnswer);
-}
-var hideAnswer = setInterval(hide, 4800);
+  }
+  var hideAnswer = setInterval(hide, 4800);
 
-//pushes question out of array to show next one
-function queue() {
-quizQuestions.shift();
-}
-//time outs and displays for questions 
-var startQuestions = setInterval(questionQueue, 0000);
-var startQueue = setInterval(queue, 5000);
-//what displays the question and answers
+  //pushes question out of array to show next one
+  function queue() {
+    quizQuestions.shift();
+  }
+  //time outs and displays for questions
+  var startQuestions = setInterval(questionQueue, 0000);
+  var startQueue = setInterval(queue, 5000);
+  //what displays the question and answers
   function questionQueue() {
     $(".quiz-display").text(quizQuestions[0].question);
     $("#a").text(quizQuestions[0].answers.a);
@@ -125,54 +122,54 @@ var startQueue = setInterval(queue, 5000);
     $("#c").text(quizQuestions[0].answers.c);
     $(".correct-answer").text(quizQuestions[0].correctAnswer);
   }
-//onclick of any response shows answer
-    $("#a").click(function(){
+  //onclick of any response shows answer
+  $("#a").click(function() {
     $(".correct-answer").show(quizQuestions[0].correctAnswer);
 
     if (quizQuestions[0].answers.a === quizQuestions[0].correctAnswer) {
-     console.log("correct");
+      console.log("correct");
       correct++;
       $(".correct-counter").text(correct);
     } else {
-        console.log("wrong");
+      console.log("wrong");
     }
-    
+
   });
 
-    $("#b").click(function(){
-      $(".correct-answer").show(quizQuestions[0].correctAnswer);
+  $("#b").click(function() {
+    $(".correct-answer").show(quizQuestions[0].correctAnswer);
 
-   if (quizQuestions[0].answers.b === quizQuestions[0].correctAnswer) {
-        console.log("correct");
-        correct++;
-        $(".correct-counter").text(correct);
-  
-        } else {
-          console.log("wrong");
-      }
+    if (quizQuestions[0].answers.b === quizQuestions[0].correctAnswer) {
+      console.log("correct");
+      correct++;
+      $(".correct-counter").text(correct);
 
-    });
+    } else {
+      console.log("wrong");
+    }
+
+  });
 
 
-    $("#c").click(function(){
-      $(".correct-answer").show(quizQuestions[0].correctAnswer);
+  $("#c").click(function() {
+    $(".correct-answer").show(quizQuestions[0].correctAnswer);
 
-      if (quizQuestions[0].answers.c === quizQuestions[0].correctAnswer) {
-        console.log("correct");
-        correct++;
-        $(".correct-counter").text(correct);
-  
-      } else {
-          console.log("wrong");
-      }
+    if (quizQuestions[0].answers.c === quizQuestions[0].correctAnswer) {
+      console.log("correct");
+      correct++;
+      $(".correct-counter").text(correct);
 
-    });
+    } else {
+      console.log("wrong");
+    }
 
-    //correct counter --counting every button press
-    $(".correct-counter").text(correct);
-    console.log(correct);
+  });
 
-//End queue display showing trivia is over
+  //correct counter --counting every button press
+  $(".correct-counter").text(correct);
+  console.log(correct);
+
+  //End queue display showing trivia is over
   var endQuestions = setTimeout(endQuiz, 20000);
 
   function endQuiz() {
@@ -182,9 +179,7 @@ var startQueue = setInterval(queue, 5000);
     $(".button-display").hide();
     $(".score").show();
     $(".wrong-counter").text(wrong);
-    
+
   }
 
-}//stat game function closure
-
-
+} //stat game function closure
